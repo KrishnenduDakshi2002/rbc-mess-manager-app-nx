06d8f79129c990def7c82cc4581ac976edcb49a2
Debayan <pradhandebayan@gmail.com>
2023-08-17 22:42:26 +0530

    added room members section

--- apps/rbc-mess-manager/src/app/navigation/RoomStack.tsx
+++ apps/rbc-mess-manager/src/app/navigation/RoomStack.tsx
@@ -1,13 +1,19 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import RoomScreen from '../screens/Room';
+import RoomMembers from '../screens/RoomMembers';
 
 const RoomStack = createNativeStackNavigator();
 
 export const RoomStackScreen = () => {
   return (
-    <RoomStack.Navigator>
+    <RoomStack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <RoomStack.Screen name="Room" component={RoomScreen} />
+      <RoomStack.Screen name="RoomMembers" component={RoomMembers} />
     </RoomStack.Navigator>
   );
 };
--- apps/rbc-mess-manager/src/app/screens/Room/index.tsx
+++ apps/rbc-mess-manager/src/app/screens/Room/index.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { theme } from '../../global/themes';
 
+import { roomData } from '../../data/roomData';
+
+import { useNavigation } from '@react-navigation/native';
+
 const RoomScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
+        height: '100%',
+        width: '100%',
+        backgroundColor: theme.colors.background,
       }}
     >
       <View
@@ -19,9 +35,51 @@ const RoomScreen = () => {
           justifyContent: 'center',
           alignItems: 'center',
           backgroundColor: theme.colors.background,
+          paddingTop: 10,
+          marginBottom: 80,
         }}
       >
-        <Text>Room Screen</Text>
+        <FlatList
+          style={{
+            flex: 1,
+            width: '90%',
+            backgroundColor: theme.colors.background,
+            // borderColor: 'red',
+            // borderWidth: 1,
+          }}
+          data={roomData}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={{
+                flex: 1,
+                height: 80,
+                borderRadius: 10,
+                width: '100%',
+                backgroundColor: theme.colors.foreground,
+                borderColor: theme.colors.textprimary,
+                borderWidth: 1,
+                justifyContent: 'flex-start',
+                alignItems: 'baseline',
+                padding: 10,
+                marginBottom: 10,
+              }}
+              onPress={() => {
+                console.log('RoomScreen: ', item.name);
+                navigation.navigate('RoomMembers');
+              }}
+            >
+              <Text
+                style={{
+                  color: theme.colors.textprimary,
+                  fontSize: 20,
+                }}
+              >
+                {item.name}
+              </Text>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item) => item.id}
+        />
       </View>
     </SafeAreaView>
   );
