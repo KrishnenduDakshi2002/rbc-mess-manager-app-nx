1d829ea0cb2e5cae7eebd98c81dfaaa3e66191b2
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-08-17 10:57:31 +0530

    github actions added for deployment
    github actions added for deployment
    
    Update deploy-api.yaml
    
    Update deploy-api.yaml
    
    Update deploy-api.yaml
    
    Update deploy-api.yaml
    
    added path filtering
    
    Update app.service.ts
    
    Update deploy-api.yaml
    
    Update app.service.ts

--- apps/rbc-mess-api/src/app/app.service.ts
+++ apps/rbc-mess-api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello RBC - This is our RBC Mess API' };
+    return { message: 'Hello RBC - This is our RBC Mess API for mess management' };
   }
 }
