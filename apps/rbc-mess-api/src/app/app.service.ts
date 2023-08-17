52ee50b760571116b75aa2b8d891ac19c80e5a4f
KrishnenduDakshi2002 <kdakshi2020@gmail.com>
2023-08-17 21:56:13 +0530

    added env for .gitignore

--- apps/rbc-mess-api/src/app/app.service.ts
+++ apps/rbc-mess-api/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import 'dotenv/config';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Hello RBC - This is our RBC Mess API for mess management' };
+    return {
+      message: `Hello RBC - This is our RBC Mess API ${process.env.S3_BUCKET}`,
+    };
   }
 }
